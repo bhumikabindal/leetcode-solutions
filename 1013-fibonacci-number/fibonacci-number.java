587a921a0612080e396be16e4bb0a43b59135d30
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-03-24 14:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (90.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,39 @@
-class Solution {
-    public int fib(int n) {
-       if(n==0){
+// class Solution {
+//     public int fib(int n) {
+//        if(n==0){
+//         return 0;
+//        } 
+//        if(n==1){
+//         return 1;
+
+//        }
+//        int ans=fib(n-1)+fib(n-2);
+//        return ans;
+//     }
+// }
+class Solution{
+    public int fibMem(int n,int[]dp){
+         if(n==0){
         return 0;
        } 
        if(n==1){
         return 1;
 
        }
-       int ans=fib(n-1)+fib(n-2);
-       return ans;
+       if(dp[n]!=-1){
+        return dp[n];
+       }
+       dp[n]=fibMem(n-1,dp)+fibMem(n-2,dp);
+       return dp[n];
+    }
+    public int fib(int n){
+        //memorization
+        //crearte dp array
+        int dp[]=new int[n+1];
+      
+            Arrays.fill(dp,-1);
+        
+        return fibMem(n,dp);
+
     }
 }

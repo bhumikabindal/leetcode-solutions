fa1c41ca00e9ba4796b8ec5d3ff51a87434bdf66
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-07-28 14:45:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,60 +1,46 @@
 class Solution {
-    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
         // code here
-        ArrayList <Integer> union= new ArrayList<>();
-        int i=0,j=0;
-        int n1=a.length;
-        int n2=b.length;
-        while(i<n1&&j<n2){
-            if(a[i]<b[j]){
-                if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
-                    union.add(a[i]);
-                    
-                    
-                }i++;
-            }
-        
-        
-             else if(a[i]>b[j]){
-                if(union.isEmpty()||union.get(union.size()-1)!=b[j]){
-                    union.add(b[j]);
-                
-                    
-                }j++;
-            }
-        
-             else{//a[i]==b[j]
-                if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
-                    union.add(a[i]);
+
+        ArrayList<Integer> ans=new ArrayList<>();
+int i=0;
+int j=0;
+        int al=a.length;
+        int bl=b.length;
+       while(i<al && j<bl){
+           if(a[i]<b[j]){
+               if(ans.size()==0 || ans.get(ans.size()-1)!=a[i]){
+                   ans.add(a[i]);
+               }
+               i++;
+           }
+                      else if(a[i]>b[j]){
+               if(ans.size()==0 || ans.get(ans.size()-1)!=b[j]){
+                   ans.add(b[j]);
+               }j++;
+           }
+           else{
+                if(ans.size()==0 || ans.get(ans.size()-1)!=b[j]){
+                   ans.add(b[j]);
                    
-                    
-                } i++;
-                    j++;
-            }
-        
-        
-        
-        
+               
+           }
+           i++;
+                   j++;
+       }
     }
-    while(i<n1){
-         if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
-             union.add(a[i]);
-
-         }
-                     i++;
-    }
-         
-    
-        while(j<n2){
-         if(union.isEmpty()||union.get(union.size()-1)!=b[j]){
-             union.add(b[j]);
-
-         }
-                         j++;
-                         
-        }
-         return union;
-         
-    }
+    while(i<al){
+        if(ans.size()==0 || ans.get(ans.size()-1)!=a[i]){
+                   ans.add(a[i]);
+                  
+    } i++;
+}
+while(j<bl){
+    if(ans.size()==0 || ans.get(ans.size()-1)!=b[j]){
+                   ans.add(b[j]);
+               }j++;
+}
+   return ans; 
+}
     
 }

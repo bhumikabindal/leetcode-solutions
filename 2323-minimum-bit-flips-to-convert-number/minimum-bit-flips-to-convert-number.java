5bb977f85bc43a07a8b1ab24abbaafb415faba10
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-08-06 13:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.76%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-       int ans=start^goal;
-     int count=0;
-       while(ans>0){
-        //brian kernighans algo
-        ans=ans & (ans-1);
-          count++;
-
-       } 
-       return count;
+        int ans=start^goal;
+      int count=0;
+        for(int i=0;i<31;i++){
+      count+=(ans & 1);
+     ans= ans>>1;
+        }
+        return count;
     }
 }

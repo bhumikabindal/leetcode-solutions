8b45b8ac4cca3ba80179be76738eee772108510c
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-08-08 15:05:26 +0530

    Time: 5 ms (81.23%) | Memory: 45.8 MB (87.74%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,16 +3,19 @@ class Solution {
         int ans=0;
         int ld;
         int check=x;
+        if(x<0)return false;
       while(x>0){
       ld=x%10;
+       if(ans>Integer.MAX_VALUE/10 || ans==Integer.MAX_VALUE/10 && ld>7){
+              return false;
+            }
+                      else if(ans<Integer.MIN_VALUE/10 || ans==Integer.MIN_VALUE/10 && ld<-8){
+              return false;
+            }
       ans=(ans*10)+ld;
       x=x/10;
       }
-      if(ans==check){
-        return true;
-      }
-
-
-        return false;
+      return ans==check;
+      
     }
 }

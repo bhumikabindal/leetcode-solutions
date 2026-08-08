77a7757e408a6aee3d87c3fe2890115d3ccc399e
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-08-08 22:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (47.92%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double ans=1;
+        long N=n;
+        if(N<0){
+            x=1.0/x;
+            N=-N;
+        }
+        while(N!=0){
+            if(N%2==1){
+                // if the pow is odd
+                // then rmeove one one pow from there
+                ans=ans*x;
+                N=N-1;
+            }
+            else{
+                N=N/2;
+                x=x*x;
+
+            }
+        }
+       
+         return ans;
     }
 }

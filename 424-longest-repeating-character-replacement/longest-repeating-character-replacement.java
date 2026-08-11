b7dbafc661739f717f704b43e772da6ce94f76c9
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-08-11 17:58:35 +0530

    Time: 47 ms (8.28%) | Memory: 47.5 MB (7.34%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,7 +5,7 @@ class Solution {
         while(r<s.length()){
             mpp.put(s.charAt(r),mpp.getOrDefault(s.charAt(r),0)+1);
             maxfreq=Math.max(maxfreq,mpp.get(s.charAt(r)));
-            while((r-l+1)-maxfreq>k){
+            if((r-l+1)-maxfreq>k){
               mpp.put(s.charAt(l),mpp.get(s.charAt(l))-1);
               if(mpp.get(s.charAt(l))==0){
                 mpp.remove(s.charAt(l));

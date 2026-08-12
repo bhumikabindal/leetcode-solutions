d8efe56f642259e807067fdc40a24dea380c8829
Bhumika Bindal <bhumikabindal60@gmail.com>
2026-08-12 14:32:17 +0530

    Time: 14 ms (51.69%) | Memory: 59.8 MB (27.86%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,17 +1,20 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        HashMap<Integer,Integer> mpp=new HashMap<>();
-        mpp.put(0,1);
-        int prefcount=0,count=0;
-        for(int num:nums){
-           if((num%2)!=0){
-            prefcount+=1;
-           }
-           if(mpp.containsKey(prefcount-k)){
-            count+=mpp.get(prefcount-k);
-           }
-           mpp.put(prefcount,mpp.getOrDefault(prefcount,0)+1);
+      public int func(int nums[],int k){
+        int l=0,r=0,count=0,ans=0;
+        while(r<nums.length){
+            if((nums[r]%2)!=0)count=count+1;
+            while(count>k){
+                if((nums[l]%2)!=0)count=count-1;
+                l=l+1;
+            }
+            if(count<=k){
+                ans=ans+(r-l+1);
+            }
+            r++;
         }
-        return count;
+        return ans;
+      }
+    public int numberOfSubarrays(int[] nums, int k) {
+        return func(nums,k)-func(nums,k-1);
     }
 }
